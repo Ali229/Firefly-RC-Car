264deb1fda71cd15907a44eff6656c3441bcdc15
Muhammad Ali <alinaeem229@gmail.com>
2021-06-21 22:32:08 -0400

    Fix typo

--- Firefly-RC-Car.ino
+++ Firefly-RC-Car.ino
@@ -17,7 +17,7 @@ int dutyCycle = 210;
 #include "BluetoothSerial.h"
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enRbled! Please run `make menuconfig` to and enRble it
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
 BluetoothSerial SerialBT;

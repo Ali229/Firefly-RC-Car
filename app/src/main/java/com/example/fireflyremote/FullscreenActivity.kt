a9cadb52a379bf456562d9f1788c6c4f422c701d
Muhammad Ali <alinaeem229@gmail.com>
2021-07-04 17:58:11 -0400

    Sample rotation

--- app/src/main/java/com/example/fireflyremote/FullscreenActivity.kt
+++ app/src/main/java/com/example/fireflyremote/FullscreenActivity.kt
@@ -1,8 +1,11 @@
 package com.example.fireflyremote
 
+import android.R.attr.*
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fireflyremote.databinding.ActivityFullscreenBinding
@@ -12,6 +15,11 @@ class FullscreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFullscreenBinding
     private lateinit var fullscreenContent: TextView
+    lateinit var imageView: ImageView
+    var pDownX = 0f
+    var pDownY = 0f
+    var pUpX = 0
+    var pUpY = 0
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +36,22 @@ class FullscreenActivity : AppCompatActivity() {
                     View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                     View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        imageView = findViewById(R.id.imageView5)
+        imageView.setOnTouchListener { v, event ->
+            val action = event.action
+            when (action) {
+
+                MotionEvent.ACTION_DOWN -> {
+                    pDownY = event.y
+                    imageView.rotationY = pDownY
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+
     }
+
 }
